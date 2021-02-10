39897bdf0b4785e6532b4a20ca5b2c2f721438bd
laortizt <adri26.ortiz@gmail.com>
2021-02-09 23:00:06 -0500

    reto 22

---
+++ script.js
@@ -0,0 +1,18 @@
+const contents = document.querySelectorAll('')
+const listItems = document.querySelectorAll('')
+
+listItems.forEach((item, idx) => {
+    item.addEventListener('', () => {
+
+        item.classList.add('')
+    })
+})
+
+function hideAllContents() {
+
+}
+
+
+function hideAllItems() {
+ 
+}
