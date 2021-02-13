44f2ae7856c287725996ebf31de237f173ee69ac
laortizt <adri26.ortiz@gmail.com>
2021-02-13 13:48:13 -0500

    OK

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const contents = document.querySelectorAll('')
-const listItems = document.querySelectorAll('')
+const contents = document.querySelectorAll('content')//se agrega content
+const listItems = document.querySelectorAll('nav ul li')// se agrega nav ul li
 
 listItems.forEach((item, idx) => {
-    item.addEventListener('', () => {
+    item.addEventListener('click', () => { //se agrega click
+        hideAllContents()//SE AGREGA
+        hideAllItems()//SE AGREGA
 
-        item.classList.add('')
+        item.classList.add('active') //se agrega active
+        contents[idx].classList.add('show')//aqui
     })
 })
 
 function hideAllContents() {
-
+    contents.forEach(content => content.classList.remove('show'))
 }
 
 
 function hideAllItems() {
- 
+    listItems.forEach(item => item.classList.remove('active'))
 }
+
